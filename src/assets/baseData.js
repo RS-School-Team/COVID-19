f88e1b935924b92e648fb1a9e1a363f06433b337
Diana <dianavaleeva@yahoo.com>
2020-12-19 21:40:43 +0300

    fix: fix markdown

--- src/assets/baseData.js
+++ src/assets/baseData.js
@@ -1,5 +1,19 @@
 const baseData = {
   dayInfo: {},
+  sortTypes: [
+    ['confirmed', 'Total Confirmed'],
+    ['death', 'Total Death'],
+    ['recovered', 'Total Recovered'],
+    ['totalConfirmedPOHT', 'Total Comfirmed Per 100,000 Population'],
+    ['deathPOHT', 'Total Deaths Per 100,000 Population'],
+    ['recoveredPOHT', 'Total Recovered Per 100,000 Population'],
+    ['newConfirmed', 'Last Day Confirmed'],
+    ['newDeaths', 'Last Day Death'],
+    ['newRecovered', 'Last Day Recovered'],
+    ['newConfirmedPOHT', 'Last Day Comfirmed Per 100,000 Population'],
+    ['newDeathPOHT', 'Last Day Death Per 100,000 Population'],
+    ['newRecoveredPOHT', 'Last Day Recovered Per 100,000 Population'],
+  ],
 };
 
 const keys = {
